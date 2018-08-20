7898fc42dc3cd2fb2e5a1f5be9de7c16c1bec783
xuzg <xzg.nju@gmail.com>
2018-08-20 08:10:55 +0800

    add test page

---
+++ page/post_page.py
@@ -0,0 +1,12 @@
+from src.page import Page
+from panel.post_panel import PostPanel1
+from panel.post_panel import PostPanel2
+
+
+class PostPage(Page):
+    init_panels =  [PostPanel1, PostPanel2]
+    url = 'http://192.168.30.115:8000/test.html'
+    kind = 'page'
+
+    def __init__(self):
+        super(PostPage, self).__init__()
