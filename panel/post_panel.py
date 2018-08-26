5264aa54bc6600711c096e123fac3e81a78b79a9
xuzg <xzg.nju@gmail.com>
2018-08-26 23:58:10 +0800

    add panle example

---
+++ panel/post_panel.py
@@ -0,0 +1,30 @@
+from src.panle import Panel
+from src.uidefinition import UIDefinition
+
+
+class PostPanel2(Panel):
+    definition = UIDefinition().root('body div:nth-child(2)', 'post2') \
+        .with_descendant('h2', 'title') \
+        .with_descendant('input', 'input') \
+        .with_descendant('li:nth-child(1)', 'comment1') \
+        .with_descendant('li:nth-child(2)', 'comment2') \
+        .with_descendant('li:nth-child(3)', 'comment3')
+
+    def __init__(self):
+        super(PostPanel2, self).__init__()
+
+
+class PostPanel1(Panel):
+    definition = UIDefinition().root('body div:nth-child(1)', 'post1') \
+        .with_descendant('h2', 'title') \
+        .with_descendant('input', 'input') \
+        .with_descendant('li:nth-child(1)', 'comment1') \
+        .with_descendant('li:nth-child(2)', 'comment2') \
+        .with_descendant('li:nth-child(3)', 'comment3')
+        # .with_descendant(PostPanel2, 'post_panel2')
+
+    def __init__(self):
+        super(PostPanel1, self).__init__()
+
+
+
---
+++ panel/todo_panle.py
@@ -0,0 +1,41 @@
+from src.panle import Panel
+from src.uidefinition import UIDefinition
+
+
+class AndPanel(Panel):
+    definition = UIDefinition().root('div.add', 'and') \
+        .with_descendant('div.and1', 'and1') \
+        .with_descendant('div.and2', 'and2') \
+        .withd_escendant('div.and3')
+    def __init__(self):
+        super(AndPanel, self).__init__()
+
+
+class TestPanel(Panel):
+
+    definition = UIDefinition().root('test', 'test') \
+        .with_descendant('test') \
+        .with_descendant('test2') \
+        .with_descendant('test3') \
+        .with_descendant(AndPanel)
+
+    def __init__(self):
+        super(TestPanel, self).__init__()
+
+
+class TodoApp(Panel):
+
+    definition = UIDefinition().root('div.app', 'app') \
+        .with_descendant('div.sa', 'title') \
+        .with_descendant('div.cd') \
+        .with_descendant('div.wd') \
+        .with_descendant(TestPanel 'post_panle2')
+
+    def __init__(self):
+        super(TodoApp, self).__init__()
+
+
+
+
+
+
