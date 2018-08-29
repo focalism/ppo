6eb62c16a55beb4e80ebe4551c0b889fc6228b0d
xuzg <xzg.nju@gmail.com>
2018-08-29 08:09:50 +0800

    modify paraments

--- src/common.py
+++ src/common.py
@@ -1,17 +1,16 @@
 class BrowserOptions:
+    def __init__(self,configs):
+        self.browser_name = configs['browser_name']
+        self.headless = configs['headless']
+        self.screen_shot = configs['screen_shot']
+        self.report_dir = configs['report_dir']
+        self.wind_size_x = configs['wind_size_x']
+        self.wind_size_y = configs['wind_size_y']
+        self.executable_path = configs['executable_path']
 
-    def __init__(self,
-                 browser_name=None,
-                 headless=False,
-                 screen_shot=False,
-                 report_dir=None,
-                 wind_size_x=0,
-                 wind_size_y=0,
-                 executable_path=None):
-        self.browser_name = browser_name
-        self.headless = headless
-        self.screen_shot = screen_shot
-        self.report_dir = report_dir
-        self.wind_size_x = wind_size_x
-        self.wind_size_y = wind_size_y
-        self.executable_path = executable_path
+class Browser:
+    def __init__(self, browser, excutor_path, options):
+        self.executor_path = excutor_path
+        self.options = options
+        self.browser = browser
+
