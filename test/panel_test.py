cf28455cf58f3e4821ddcf4943acf395165e18a3
xuzg <xzg.nju@gmail.com>
2018-08-31 00:47:41 +0800

    modify test case

--- test/panel_test.py
+++ test/panel_test.py
@@ -1,16 +1,22 @@
 from panel.post_panel import PostPanel1
 from src.context import Context
 
-def test_find_ui_node(fixture_session: Context):
-    fixture_session.browser.get('http://192.168.30.115:8000/test.html')
-    panel = fixture_session.wait_for(PostPanel1)
-    node = panel.find_ui_node('post1')
-    assert (node['selector'] == 'body div:nth-child(1)')
-    assert (node['name'] == 'post1')
-
-def test_click_element(fixture_session: Context):
-    panel = fixture_session.wait_for(PostPanel1)
+def test_click_element(fixture_function: Context):
+    fixture_function.browser.get('http://192.168.30.115:8000/test.html')
+    panel = fixture_function.wait_for(PostPanel1)
     node = panel.find_ui_node('title')
     assert (node['selector'] == 'body div:nth-child(1) h2')
     assert (node['name'] == 'title')
     panel.click('link')
+
+
+
+def test_find_ui_node(fixture_function: Context):
+    fixture_function.browser.get('http://192.168.30.115:8000/test.html')
+    panel = fixture_function.wait_for(PostPanel1)
+    node = panel.find_ui_node('post1')
+    assert (node['selector'] == 'body div:nth-child(1)')
+    assert (node['name'] == 'post1')
+
+
+
