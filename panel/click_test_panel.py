20943daf17951a89f0f70b33e82b580f80561561
xuzg <xzg.nju@gmail.com>
2018-09-03 00:17:13 +0800

    add test pane;

---
+++ panel/click_test_panel.py
@@ -0,0 +1,23 @@
+from src.panel import Panel
+from src.uidefinition import UIDefinition
+
+
+class ClickPanel(Panel):
+
+    definition = UIDefinition().root('div')\
+        .with_descendant('#double_click', 'double')\
+        .with_descendant('#single_click','single')\
+        .with_descendant('#jump', 'jump')\
+        .with_descendant('#jump_new_page', 'jump new')
+
+class LoginPanel(Panel):
+
+    definition = UIDefinition().root('#login')\
+        .with_descendant("#name", 'name')\
+        .with_descendant("#email", 'email')
+
+
+class MouseOverPanel(Panel):
+
+    definition = UIDefinition().root('#hover')\
+        .with_descendant('#mouse_hover','hover')
